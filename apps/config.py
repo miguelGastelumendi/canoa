362047f1d77495e1e1bda9d3bc95f6653ad91478
assismauro <assismauro@hotmail.com>
2022-12-14 11:09:41 -0300

    Imagens de topografia em static/assets/img/topografia

--- apps/config.py
+++ apps/config.py
@@ -22,18 +22,16 @@ class Config(object):
     Calc URI based on pyodbc string connection:
 
 from sqlalchemy.engine import URL
-connection_string='DRIVER={ODBC Driver 18 for SQL Server};SERVER=54.207.37.232,30310;DATABASE=RURALLEGAL;ENCRYPT=no;UID=rlatrium;PWD=Hibisco@12'
+connection_string='DRIVER={ODBC Driver 18 for SQL Server};SERVER=<IP>,<PORT>;DATABASE=<DATABASE>;ENCRYPT=no;UID=<USER>;PWD=<PASSWORD>'
 connection_url = URL.create("mssql+pyodbc", query={"odbc_connect": connection_string})
 # testing:
 from sqlalchemy.engine import create_engine
 engine = create_engine(connection_url)
 
     Original string:
-    SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://mauro:Araticum@12@data.atriumforest.com.br:/APPSPEED?driver=ODBC+Driver+18+for+SQL+Server'
+    SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://<USER>:<PASSWORD>@<SERVER>:/APPSPEED?driver=ODBC+Driver+18+for+SQL+Server'
+    SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://?odbc_connect=DRIVER%3D%7BODBC+Driver+17+for+SQL+Server%7D%3B<SERVER>%3D<SERVER>%2C30310%3BDATABASE%<DATABASE>%3BENCRYPT%3Dno%3BUID%3D<USER>%3BPWD%3D<PASSWORD>'
     '''
-    # SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://?odbc_connect=DRIVER%3D%7BODBC+Driver+18+for+SQL+Server%7D%3BSERVER%3Dtcp%3A54.207.37.232%2C30310%3BDATABASE%3DRURALLEGAL%3BENCRYPT%3Dno%3BUID%rlatrium%3BPWD%3DHibisco@12%4012'
-    # SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://?odbc_connect=DRIVER%3D%7BODBC+Driver+18+for+SQL+Server%7D%3BSERVER%3D54.207.37.232%2C30310%3BDATABASE%3DRURALLEGAL%3BENCRYPT%3Dno%3BUID%3Drlatrium%3BPWD%3DHibisco%4012'ALCHEM
-    # SQLALCHEMY_DATABASE_URI =   'mssql+pyodbc://?odbc_connect=DRIVER%3D%7BODBC+Driver+17+for+SQL+Server%7D%3BSERVER%3D54.207.37.232%2C30310%3BDATABASE%3DRURALLEGAL%3BENCRYPT%3Dno%3BUID%3Drlatrium%3BPWD%3DHibisco%4012'
     SQLALCHEMY_DATABASE_URI = os.environ['SQLALCHEMY_DATABASE_URI']
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
