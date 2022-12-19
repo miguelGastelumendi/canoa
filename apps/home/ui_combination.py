524669cbf16a7ff9cb24037fefc42e452cce5c4a
assismauro <assismauro@hotmail.com>
2022-12-19 07:05:39 -0300

    Mostrando fluxo de caixa real.

--- apps/home/ui_combination.py
+++ apps/home/ui_combination.py
@@ -2,18 +2,22 @@ from apps.home import dbquery as dbquery
 
 def getCombinations(idProjeto):
     df = dbquery.getDataframeResultset(
-        f"select ft.nomeFaixa,  vfc.idFaixaTipo,  "
-        f"vfc.idCombinacao, vfc.Especies, "
-        f"vfc.TIR, vfc.payback, vfc.InvNecessario, VTLiquido, vfc.VPLiquido "
+        f"select ft.nomeFaixa,  vfc.idFaixaTipo,  vfc.idCombinacao, vfc.Especies, vfc.TIR, vfc.payback, vfc.InvNecessario, VTLiquido, vfc.VPLiquido "
         f"from v_filtraCombinacoes vfc "
         f"inner join FaixaTipo ft "
         f"on vfc.idFaixaTipo = ft.id "
-        f"where vfc.idFaixaTipo in (1,2) "
-        f"and vfc.idFitofisionomia = 10 "
-        f"and vfc.idRegiaoEco = 3 "
-        f"and vfc.idRegiaoAdm = 5 "
-        f"and vfc.idTopografia = 3 "
-        f"and vfc.idMecanizacaoNivel = 1 "
+        f"inner join ProjetoPreferencias pp "
+        f"on pp.idTopografia = vfc.idTopografia "
+        f"and pp.idMecanizacaoNivel = vfc.idMecanizacaoNivel "
+        f"inner join MunicipioFito mf "
+        f"on pp.idMunicipioFito = mf.id "
+        f"and vfc.idFitofisionomia = mf.idFitoFisionomia "
+        f"inner join Municipio m "
+        f"on mf.idMunicipio = m.id "
+        f"and vfc.idRegiaoEco = m.idRegiaoEco "
+        f"and vfc.idRegiaoAdm = m.idRegiaoAdm "
+        f"where vfc.idFaixaTipo in (select idFaixaTipo from ModeloFaixa mf2 where idModeloPlantio = pp.idModeloPlantio) " 
         f"and vfc.TIR > 0 "
-        f"order by nomeFaixa,TIR DESC ")
+        f"and pp.idProjeto = {idProjeto} "
+        f"order by idFaixaTipo,TIR DESC ")
     return df, df.nomeFaixa.unique().tolist()
