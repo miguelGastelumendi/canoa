ee167a5c19153e6b40b46980267eee4639336548
Miguel Gastelumendi <miguelgastelumendi@hotmail.com>
2022-12-24 14:06:28 -0300

    Wizard version 0.7

--- apps/static/assets/js/wizard.js
+++ apps/static/assets/js/wizard.js
@@ -1,7 +1,11 @@
-//  wizard.js
-//  Miguel Gastelumendi version 0.2.0 2022.12.22 -- 23
+/**
+ * @preserve
+ * Wizard js
+ * version 0.7
+ * 2022.12.22--24
+ * Miguel Gastelumendi -- mgd
+*/
 // @ts-check
-
 
 /**
  * This callback should return true to go
@@ -11,18 +15,18 @@
 
 /**
  * @typedef {Object} wzdControl
- * @param {Function} messageInfo
- * @param {Function} messageError
+ * @property {Function} messageInfo
+ * @property {Function} messageError
  * :
  * :
 */
-//const mdlControl = {};
 
 const wzdControl = {
-  selectedBtnIx: -1,
+  selectedItemIx: -1,
   nextPageHref: '',  // Wizard page has ony one target
-  jsoData: null,
   displayMode: 0,
+  /** @type {Array<wzdItem>} */
+  jsoData: [],
   /** @type {fOnNext?} */
   fOnNext: null,
   ge: (/** @type {string} */ sId) => /** @type{HTMLElement} */(document.getElementById(sId)),
@@ -30,39 +34,42 @@ const wzdControl = {
   getBtnId: (/** @type {number} */ ix) => `btn${ix}`,
   setBody: (/** @type {string} */ sBody) => wzdControl.ge('idWzdBody').innerHTML = '' + sBody,
 
+  //@ts-ignore mdlControl is defined on modal.js
   modalReady: () => (typeof mdlControl == 'object'),
+
+  /** @private */
   showSelected: (/** @type {number} */ ix) => {
-    const eleUsDisplay = wzdControl.ge('idWzdUsDisplay');
-    eleUsDisplay.innerHTML = `Selecionado: <b>${wzdControl.jsoData[ix].caption}</b>`;
-    eleUsDisplay.classList.remove('visually-hidden');
+    wzdControl.displaySelected(`Selecionado: <b>${wzdControl.jsoData[ix].caption}</b>`);
   },
-  //-- Button select
-  selectBtn: (/** @type {number} */ ix) => {
-    if (wzdControl.selectedBtnIx == ix) { return; }
+
+  /** @private */
+  selectItem: (/** @type {number} */ ix) => {
+    if (wzdControl.selectedItemIx == ix) { return; }
     wzdControl.showSelected(ix);
     let eleBtn = wzdControl.getBtn(ix)
     eleBtn.classList.remove('btn-info');
     eleBtn.classList.add('btn-warning');
-    if (wzdControl.selectedBtnIx >= 0) {
-      eleBtn = wzdControl.getBtn(wzdControl.selectedBtnIx);
+    if (wzdControl.selectedItemIx >= 0) {
+      eleBtn = wzdControl.getBtn(wzdControl.selectedItemIx);
       eleBtn.classList.remove('btn-warning');
       eleBtn.classList.add('btn-info');
     }
-    wzdControl.selectedBtnIx = ix;
+    wzdControl.selectedItemIx = ix;
   },
+
+  /** @private */
   display: () => {
     const sPath = '../../static/assets/img/modelo_plantio/';
     const htmCol = '<div class="col text-center mb-3">';
     let sHtml;
     switch (wzdControl.displayMode) {
       case wzdControl.mode.CUSTOM:
-        sHtml = '<div>';
-        break;
+        return;
       case wzdControl.mode.BUTTONS:
         sHtml = '<div class="d-grid gap-2">';
         wzdControl.jsoData.forEach((itm, i) => {
           sHtml +=
-            `<button id="${wzdControl.getBtnId(i)}" class="btn btn-info bg-gradient mx-5" type="button" onclick="wzdControl.selectBtn(${i})">` +
+            `<button id="${wzdControl.getBtnId(i)}" class="btn btn-info bg-gradient mx-5" type="button" onclick="wzdControl.selectItem(${i})">` +
             itm.caption +
             '</button>';
         });
@@ -70,10 +77,10 @@ const wzdControl = {
       case wzdControl.mode.IMAGES:
         sHtml = /* htmlRow */ '<div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4">';
         wzdControl.jsoData.forEach((itm, i) => {
-          let onClick = `onclick="wzdControl.selectImg(${i})`;
+          let onClick = `onclick="wzdControl.selectItem(${i})`;
           sHtml += htmCol + // <htmCol>
-            `<button class="mb-2 btn btn-${(i == wzdControl.selectedBtnIx) ? 'warning' : 'info'} bg-gradient" type="button" style="height:4.2em; width:100%" ${onClick}">${itm.caption}</button>` +
-            `<a ${onClick}"> <img src="${sPath}${itm.fileName}.png" alt="${itm.fileName}"></a>` +
+            `<button id=${wzdControl.getBtnId(i)} class="mb-2 btn btn-${(i == wzdControl.selectedItemIx) ? 'warning' : 'info'} bg-gradient" type="button" style="height:4.2em; width:100%" ${onClick}">${itm.caption}</button>` +
+            `<a ${onClick}"> <img src="${sPath}${itm.fileName}.png" alt="${itm.fileName ? itm.fileName : 'Imagem não disponível'}"></a>` +
             '</div>'; /* </htmCol>  */
         });
         break;
@@ -83,16 +90,18 @@ const wzdControl = {
     sHtml += '</div>'; // htmlRow
     wzdControl.setBody(sHtml)
   },
-  next : () => {
-    let sHref= '';
+
+  /** @private */
+  gotoNextPage: () => {
+    let sHref = '';
     let jsBtn
-    if (wzdControl.fOnNext && !wzdControl.fOnNext()){
+    if (wzdControl.fOnNext && !wzdControl.fOnNext()) {
       // Not this time 8-|
     } else if (wzdControl.displayMode == wzdControl.mode.CUSTOM) {
       sHref = wzdControl.nextPageHref;
-    } else if (wzdControl.selectedBtnIx < 0) {
+    } else if (wzdControl.selectedItemIx < 0) {
       wzdControl.messageInfo('Por favor, selecione uma das opções.')
-    } else if ((jsBtn = wzdControl.jsoData[wzdControl.selectedBtnIx]).href) {
+    } else if ((jsBtn = wzdControl.jsoData[wzdControl.selectedItemIx]).href) {
       sHref = jsBtn.href;
     } else if (!wzdControl.nextPageHref) {
       wzdControl.messageError(`Não está definido o próximo passo de '${jsBtn.caption}'.`)
@@ -101,8 +110,10 @@ const wzdControl = {
     } else {
       sHref = jsBtn.href || wzdControl.nextPageHref;
     }
-    if (sHref) window.location.href = sHref;
+    if (sHref) setTimeout(() => { window.location.href = sHref }, 0);
   },
+
+  /** @public */
   messageInfo: (sMsg, sTitle) => {
     if (wzdControl.modalReady()) {
       mdlControl.message(sMsg, sTitle)
@@ -110,6 +121,8 @@ const wzdControl = {
       alert(sMsg)
     }
   },
+
+  /** @public */
   messageError: (sMsg, sTitle, fOnError) => {
     if (fOnError) { fOnError(); }
     if (wzdControl.modalReady()) {
@@ -119,36 +132,56 @@ const wzdControl = {
     }
   },
 
-  mode: { BUTTONS: 1, IMAGES: 2, CUSTOM: 3 },
-  /* ---------------------------------
-     Client must call one of this procs
-     to initialize the Wizard
-  */
+  /**
+   * Display HTML text of selected item
+   * @param {string} sHtml
+   * @public
+   */
+
+  displaySelected: (sHtml) => {
+    const eleUsDisplay = wzdControl.ge('idWzdUsDisplay');
+    eleUsDisplay.innerHTML = '' + sHtml;
+    if (sHtml) {
+      eleUsDisplay.classList.remove('visually-hidden');
+    } else {
+      eleUsDisplay.classList.add('visually-hidden');
+    }
+  },
 
   /**
-   * Initialize wizard
-   * @param {Object} jsonData
-   * @param {string} sMode  ['buttons' | todo ...]
-   * @param {fOnNext?} [onNext]
+   * @enum {number} Wizard items display mode
+   * @public
+  */
+  mode: { BUTTONS: 1, IMAGES: 2, CUSTOM: 3 },
+
+  /**
+   * @typedef {Object} wzdItem
+   * @property {string} caption display text
+   * @property {number} id item`s ID, if informed, it is sent as a parameter
+   * @property {string} href address of the next page (if all items have the same one, use 'nextPage')
+   * @property {string} fileName image file name
    */
-  initPage: (jsonData, eMode, onNext = null) => {
-    wzdControl.jsoData = jsonData;
-    wzdControl.displayMode = eMode;
-    wzdControl.OnNext = /** @type {fOnNext} */ (onNext);
+
+  /**
+   * @typedef {Object} wzdConfig
+   * @property {Array<wzdItem>} data array of json items
+   * @property {number} mode see wzdControl.mode
+   * @property {fOnNext?} onNext callback on Next button
+   * @property {string} nextPage address of next page
+   */
+  /**
+   * Initialize wizard's page
+   * @param {wzdConfig} jsonConfig
+   * @public
+   */
+  initPage: (jsonConfig) => {
+    wzdControl.jsoData = jsonConfig.data || [];
+    wzdControl.displayMode = jsonConfig.mode;
+    wzdControl.fOnNext = jsonConfig.onNext || null;
+    wzdControl.nextPageHref = jsonConfig.nextPage || '';
     wzdControl.display();
     wzdControl.ge('idWzdBtnOk').disabled = false;
   },
 
-  /**
-   * Initialize wizard for custom, all is in your handas
-   * @param {string} sNextPage address of next page
-   * @param {fOnNext} onNext callback to accept next button
-   */
-  initCustom: (sNextPage, onNext) => {
-    wzdControl.displayMode = wzdControl.mode.CUSTOM
-    wzdControl.fOnNext = onNext;
-    wzdControl.nextPageHref = sNextPage;
-    wzdControl.ge('idWzdBtnOk').disabled = false;
-  }
 }
 //{# eof #}
