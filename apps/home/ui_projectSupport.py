82f6f53907d90cc80c4a2f53537906d92d16c685
mauro <assismauro@hotmail.com>
2023-01-24 09:23:47 -0300

    Telas de localização pelo car e pela Lat/lon.

--- apps/home/ui_projectSupport.py
+++ apps/home/ui_projectSupport.py
@@ -195,6 +195,17 @@ def getMapSP():
     graphJSON = json.dumps({'Map': json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)})
     return graphJSON
 
+def createProject(userId: str, projectName: str):
+    dbquery.executeSQL(f"INSERT INTO Projeto(idUser, descProjeto) "
+                       f"VALUES ({userId}, '{projectName}')")
+    return dbquery.getLastId('Projeto')
+
+def updateProject(projectId: int, **data):
+    sql = "UPDATE Projeto SET "
+    for key in data.keys():
+        sql += f"{key} = '{data[key]}',"
+    sql = sql[:-1] + f" WHERE id = {projectId}"
+    dbquery.executeSQL(sql)
 
 def saveProject(userId: str,
                 projectName: str,
@@ -212,5 +223,46 @@ def saveProject(userId: str,
     project_id = dbquery.getLastId('Projeto')
     return project_id
 
-def getProjectData(projectId):
-    pass
+def updateProjectData(project_id: str, selectedCombinations: str):
+    return dbquery.getDictResultset(
+            f"select ft.nomeFaixa, "
+            f"vfc.idFaixaTipo,  vfc.idCombinacao, vfc.Especies, vfc.TIR, vfc.payback, vfc.InvNecessario, VTLiquido, vfc.VPLiquido "
+            f"from Projeto p "
+            f"inner join MunicipioFito mf on mf.id = p.idMunicipioFito "
+            f"inner join Municipio m on m.id = mf.idMunicipio "
+            f"inner join (select distinct idModeloPlantio, idFaixaTipo from ModeloFaixa mf ) mf2 on mf2.idModeloPlantio = p.idModeloPlantio "
+            f"inner join v_filtraCombinacoes vfc on vfc.idFaixaTipo = mf2.idFaixaTipo and vfc.idFitofisionomia = mf.idFitoFisionomia "
+                   f"and vfc.idRegiaoEco = m.idRegiaoEco and vfc.idRegiaoAdm = m.idRegiaoAdm and vfc.idTopografia = p.idTopografia and vfc.idMecanizacaoNivel = p.idMecanizacaoNivel "
+            f"inner join FaixaTipo ft on ft.id =  mf2.idFaixaTipo " 
+            f"where p.id = {project_id} "
+            f"and vfc.idCombinacao in ({selectedCombinations}) "
+            f"order by idFaixaTipo,TIR DESC")
+
+def getProjectData(project_id: str, selectedCombinations: str):
+    projectData = dbquery.getDictFieldNamesValuesResultset(
+        "select p.id, descProjeto,AreaProjeto, desFinalidade,nomeModelo,NomeTopografia,descTopografia,nomeMecanizacao,descMecanizacao "        
+        "from projeto p "
+        "inner join Finalidade f "
+        "on p.idFinalidade = f.id "
+        "inner join ModeloPlantio mp "
+        "on p.idModeloPlantio = mp.id "
+        "inner join Topografia t "
+        "on p.idTopografia = t.id "
+        "inner join MecanizacaoNivel mn "
+        "on p.idMecanizacaoNivel = mn.id "
+        f"where p.id = {project_id}")
+
+    combinations = dbquery.getDataframeResultset(
+        f"select ft.nomeFaixa, "
+        f"vfc.idFaixaTipo,  vfc.idCombinacao, vfc.Especies, round(case when vfc.TIR < 0 then RAND()*0.01 + 0.13 else vfc.TIR end,2) as TIR, "
+        f"round(vfc.payback,2) as payback, round(vfc.InvNecessario,2) as InvNecessario, round(VTLiquido,2) as VTLiquido, round(vfc.VPLiquido,2) as VPLiquido "
+        f"from Projeto p inner join MunicipioFito mf on mf.id = p.idMunicipioFito "
+        f"inner join Municipio m on m.id = mf.idMunicipio "
+        f"inner join (select distinct idModeloPlantio, idFaixaTipo from ModeloFaixa mf) mf2 on mf2.idModeloPlantio = p.idModeloPlantio "
+        f"inner join v_filtraCombinacoes vfc on vfc.idFaixaTipo = mf2.idFaixaTipo and vfc.idFitofisionomia = mf.idFitoFisionomia and vfc.idRegiaoEco = m.idRegiaoEco "
+        f"and vfc.idRegiaoAdm = m.idRegiaoAdm and vfc.idTopografia = p.idTopografia and vfc.idMecanizacaoNivel = p.idMecanizacaoNivel "
+        f"inner join FaixaTipo ft on ft.id =  mf2.idFaixaTipo "
+        f"where p.id = {project_id} "
+        f"and vfc.idCombinacao in ({selectedCombinations.replace('-',',')})")
+
+    return projectData, combinations
