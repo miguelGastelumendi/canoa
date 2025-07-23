56fc0d214422b2474436f3501e51ea5424d29ab4
miguelGastelumendi <miguelGastelumendi@hotmail.com>
2025-07-22 22:36:15 -0300

    2025-07-21--22 β 4.65 [Published] update versions

--- carranca/common/app_constants.py
+++ carranca/common/app_constants.py
@@ -16,7 +16,7 @@ app_constants.py
 APP_NAME = "Canoa"
 
 # &beta; major.minor.patch
-APP_VERSION = "β 4.64"  # 2025-07-19
+APP_VERSION = "β 4.65"  # 2025-07-22
 
 # default user HTML/DB lang/locale (see table users.lang)
 APP_LANG = "pt-br"
